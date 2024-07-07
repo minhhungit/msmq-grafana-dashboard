cc1a7513dbd2134636ca24dc5cac038da2510b62
Jin <it.minhhung@gmail.com>
2024-07-08 00:31:29 +0700

    support outgoing queue

--- msmq-counter.ps1
+++ msmq-counter.ps1
@@ -24,6 +24,16 @@ try {
 
             # Collect MSMQ data
             $msmqData = Get-MsmqQueue | Select QueueName, MessageCount, BytesInQueue, MachineName, JournalMessageCount, Transactional
+            $outgoingMsmqData = Get-MsmqOutgoingQueue | Select DestinationQueueFormatName, MessageCount, BytesInQueue, UnacknowledgedMessageCount, UnprocessedMessageCount, ConnectionHost, Transactional, State
+
+            # Function to map state to numeric value
+            function Convert-StateToNumeric($state) {
+                switch ($state) {
+                    "MQ_QUEUE_STATE_CONNECTED" { return 1 }
+                    "MQ_QUEUE_STATE_DISCONNECTED" { return 0 }
+                    default { return -1 }
+                }
+            }
 
             # Format the data for Prometheus
             $metrics = @()
@@ -36,6 +46,22 @@ try {
                 $metrics += "msmq_queue_journal_message_count{queue_name=`"$queueName`",machine_name=`"$machineName`"} $($queue.JournalMessageCount)"
                 $metrics += "msmq_queue_transactional{queue_name=`"$queueName`",machine_name=`"$machineName`"} $([int]$queue.Transactional)"
             }
+
+            foreach ($queue in $outgoingMsmqData) {
+                $queueName = $queue.DestinationQueueFormatName -replace "\\", "\\\\"
+                $connectionHost = $queue.ConnectionHost -replace "\\", "\\\\"
+                
+                $transactional = if ($queue.Transactional -eq "MQ_XACT_STATUS_UNKNOWN") { 0 } else { [int]$queue.Transactional }
+                $stateNumeric = Convert-StateToNumeric $queue.State
+
+                $metrics += "msmq_outgoing_queue_message_count{queue_name=`"$queueName`",connection_host=`"$connectionHost`"} $($queue.MessageCount)"
+                $metrics += "msmq_outgoing_queue_bytes_in_queue{queue_name=`"$queueName`",connection_host=`"$connectionHost`"} $($queue.BytesInQueue)"
+                $metrics += "msmq_outgoing_queue_unacknowledged_message_count{queue_name=`"$queueName`",connection_host=`"$connectionHost`"} $($queue.UnacknowledgedMessageCount)"
+                $metrics += "msmq_outgoing_queue_unprocessed_message_count{queue_name=`"$queueName`",connection_host=`"$connectionHost`"} $($queue.UnprocessedMessageCount)"
+                $metrics += "msmq_outgoing_queue_transactional{queue_name=`"$queueName`",connection_host=`"$connectionHost`"} $transactional"
+                $metrics += "msmq_outgoing_queue_state{queue_name=`"$queueName`",connection_host=`"$connectionHost`"} $stateNumeric"
+            }
+
             $metricsOutput = $metrics -join "`n"
 
             # Write the metrics to the response
